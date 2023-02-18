7ad61229143d4a6063dee29983c62f17496db68c
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-02-18 17:34:22 +0530

    adding

--- contracts/CreditFi.sol
+++ contracts/CreditFi.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./safemath.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract CreditFi {
-
+    using SafeMath for uint256;
     address public owner;
+    uint public activityCount;
 
 
 
     constructor() {
         owner = msg.sender;
+        activityCount = 0;
     }
 
 
@@ -24,6 +28,7 @@ contract CreditFi {
 
     //enums
     enum CreditCategory {greenCredit, blueCredit, eduCredit, healthCredit, pinkCredit, hungerCredit}
+    enum ActivityStatus {unverified, verified}
 
     //structers
 
@@ -57,19 +62,25 @@ contract CreditFi {
 
 
     struct Activity {
+        uint activityId;
         address user;
         string title;
         string[] fileHashes;
         string description;
         CreditCategory category;
+        ActivityStatus status;
         uint upvotes;
         uint downvotes;
         uint raisedAmount;
     }
 
+    //Arrays
+    Activity[] public activities;
+
     //Events
     event userCreated(address _user, string _name);
     event organizationCreated(address _organisation, string _name);
+    event activityCreated(string _title, address _user);
 
 
     function createUser(string memory name, string memory email, string memory ph_num, string memory description) public returns (bool) {
@@ -89,9 +100,40 @@ contract CreditFi {
     }
 
 
-    // function createActivity(string memory title, string[] memory fileHashes, string memory description, uint8 category)  returns (bool) {
-        
-    // }
+    function createActivity(string memory _title, string[] memory _fileHashes, string memory _description, uint8 _category)  public returns (bool) {
+        require(bytes(_title).length >0);
+        CreditCategory _creditCategory = CreditCategory(_category);
+
+        Activity memory newactivity = Activity({
+            activityId : activityCount,
+            user: msg.sender,
+            title: _title,
+            fileHashes: _fileHashes,
+            description: _description,
+            category: _creditCategory,
+            upvotes : 0,
+            downvotes : 0,
+            raisedAmount : 0
+        });
+        activities.push(newactivity);
+        activityCount.add(1);
+        emit activityCreated(_title, msg.sender);
+        return true;
+    }
+
+    function getActivities() public view returns (Activity[] memory) {
+        return activities;
+    }
+
+    function upVote(uint _activityId) public returns (bool) {
+        activities[_activityId].upvotes += 1;
+        return true;
+    }
+
+    function downVote(uint _activityId) public returns (bool) {
+        activities[_activityId].downvotes += 1;
+        return true;
+    }
 
 
     
